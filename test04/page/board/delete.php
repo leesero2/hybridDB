987564755527c01a4930b24254397ad6c6bebd23
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-02 22:32:33 +0900

    Delete test04 directory

--- test04/page/board/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include "db.php";
-
-	$bno = $_GET['idx'];
-	$sql = mq("delete from board where idx='$bno';");
-?>
-<script type="text/javascript">alert("삭제되었습니다.");</script>
-<meta http-equiv="refresh" content="0 url=/test04/index.php?" />
